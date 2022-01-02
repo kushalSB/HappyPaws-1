41066c20e959971e8c88be6ef554cfb090872f40
samyog437 <fallendemon174@gmail.com>
2022-01-02 19:47:01 +0545

    increment buttons

--- happy paws scripts/productdetail.js
+++ happy paws scripts/productdetail.js
@@ -1,34 +1,6 @@
-$(document).ready(function(){
-
-var quantitiy=0;
-   $('.quantity-right-plus').click(function(e){
-        
-        // Stop acting like a button
-        e.preventDefault();
-        // Get the field name
-        var quantity = parseInt($('#quantity').val());
-        
-        // If is not undefined
-            
-            $('#quantity').val(quantity + 1);
-
-          
-            // Increment
-        
-    });
-
-     $('.quantity-left-minus').click(function(e){
-        // Stop acting like a button
-        e.preventDefault();
-        // Get the field name
-        var quantity = parseInt($('#quantity').val());
-        
-        // If is not undefined
-      
-            // Increment
-            if(quantity>1){
-            $('#quantity').val(quantity - 1);
-            }
-    });
-    
-});
+function increment() {
+    document.getElementById('quantity').stepUp();
+ }
+ function decrement() {
+    document.getElementById('quantity').stepDown();
+ }
