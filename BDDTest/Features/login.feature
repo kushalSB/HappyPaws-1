9292848113b40793a2abf5697a0105aec3919e64
kitarp07 <78782387+kitarp07@users.noreply.github.com>
2022-02-19 19:14:45 +0545

    Delete BDDTest directory

--- BDDTest/Features/login.feature
+++
@@ -1,7 +0,0 @@
-Feature: Test the happyPaws application
-    Scenario : verify Helppage 
-        Given Launch the browser
-        Then verify the page title
-        And close the browser
-
-    
--- BDDTest/Pages/HelpPage.py
+++
@@ -1,9 +0,0 @@
-class Helppage:
-    link_login_xpath = "//*[@id='sign']"
-
-    def __init__(self, driver):
-        self.driver = driver
-
-    def clickOnLogin(self):
-        self.driver.find_element_by_xpath(self.link_login_xpath).click()
-        
